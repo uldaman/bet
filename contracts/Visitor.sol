0b62c067caf07dff75da015508fcd0eeb490bce7
hanxiao <zhuec@hotmail.com>
2019-02-26 12:06:57 +0800

    添加 visitor 方法

--- contracts/Visitor.sol
+++ contracts/Visitor.sol
@@ -3,11 +3,19 @@ pragma solidity >=0.4.24 <0.6.0;
 import "./Player.sol";
 
 contract Visitor is Player {
-    function getLeftTotalPledge(uint _id) public view returns(uint) {
+    function getLeftTotalPledgeByGameId(uint _id) public view returns(uint) {
         return quizs[_id].totalPledge[left];
     }
 
-    function getRightTotalPledge(uint _id) public view returns(uint) {
+    function getRightTotalPledgeByGameId(uint _id) public view returns(uint) {
         return quizs[_id].totalPledge[right];
+    }
+
+    function getPledgeByGameId(uint _id) public view returns(uint) {
+        return quizs[_id].players[msg.sender].pledge;
+    }
+
+    function getOptionByGameId(uint _id) public view returns(uint) {
+        return quizs[_id].players[msg.sender].option;
     }
 }
