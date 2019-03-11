0a91fae352d2be4e164a11b821d100723866a0cc
hanxiao <zhuec@hotmail.com>
2019-03-11 12:00:25 +0800

    fix: visitor

--- contracts/Visitor.sol
+++ contracts/Visitor.sol
@@ -6,22 +6,24 @@ contract Visitor is Player {
     function getQuiz(uint _id) public view returns(
         uint id, Stages stage,
         string memory leftName, uint leftPledge,
-        string memory rightName, uint rightPledge,
-        uint winner
+        string memory rightName, uint rightPledge
     ) {
         require(quizs[_id].stage != Stages.None, "Quiz not exited");
         Quiz storage quiz = quizs[_id];
         id = _id;
         stage = quiz.stage;
-        leftName = quiz.leftName;
-        rightName = quiz.rightName;
-        winner = quiz.winner;
-        leftPledge = quiz.totalPledge[left];
-        rightPledge = quiz.totalPledge[right];
+
+        Combatant storage left = quiz.combatants[left];
+        Combatant storage right = quiz.combatants[right];
+
+        leftName = left.name;
+        rightName = right.name;
+        leftPledge = left.pledge;
+        rightPledge = right.pledge;
     }
 
-    function getPlayerChoicePledge(uint _id, uint combatant) public view returns(uint) {
+    function getPlayerCombatantPledge(uint _id, uint combatant) public view returns(uint) {
         require(combatant == left || combatant == right, "Combatant can only be 1 or 2");
-        return quizs[_id].players[msg.sender].pledge[combatant];
+        return quizs[_id].combatants[combatant].players[msg.sender].pledge;
     }
 }
