a4e8cadfbe1bd76ee5e0ddf046fcafd012afa982
hanxiao <zhuec@hotmail.com>
2019-03-12 15:42:05 +0800

    chore: 修改 migrations 脚本

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
 const Visitor = artifacts.require("Visitor");
-const UintList = artifacts.require("UintList");
 
 module.exports = function (deployer) {
-    deployer.deploy(UintList, { from: "0x5e4efedf3d71232340280d8bc475421352994b63" });
-    deployer.link(UintList, Visitor);
     deployer.deploy(Visitor, { from: "0x5e4efedf3d71232340280d8bc475421352994b63" });
 };
